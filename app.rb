1d772764bafb5db5693d26ee97692d90a758adef
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-15 00:42:42 +0900

    databaseとの接続完了

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'mysql2'
+require 'dotenv'
+require 'yaml'
+require 'erb'
 
 set :bind, '0.0.0.0'
+Dotenv.load
+yaml_file = File.read("database.yml")
+sql_client = Mysql2::Client.new(YAML.load(ERB.new(yaml_file).result))
 
 get '/' do
   'Welcome to Sinatra!!'
+  query = "show tables"
+
+  sql_client.query(query).each do |a|
+    p a
+  end
 end
