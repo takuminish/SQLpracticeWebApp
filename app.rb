ba7d1005c1815f51db11635a231ccee66e5713bc
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-15 01:15:28 +0900

    起動をRackup形式にした

--- app.rb
+++ app.rb
@@ -1,20 +1,25 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/reloader'
 require 'mysql2'
 require 'dotenv'
 require 'yaml'
 require 'erb'
 
-set :bind, '0.0.0.0'
+class SQLApplication < Sinatra::Base
+
 Dotenv.load
 yaml_file = File.read("database.yml")
 sql_client = Mysql2::Client.new(YAML.load(ERB.new(yaml_file).result))
 
-get '/' do
-  'Welcome to Sinatra!!'
-  query = "show tables"
-
-  sql_client.query(query).each do |a|
-    p a
+  
+  get '/' do
+    'Welcome to Sinatra!!'
+    query = "select * from test1"
+    output = ""
+    result = sql_client.query(query)
+    result.each do |a|
+      output =  a["text"]
+    end
+    return output
   end
 end
