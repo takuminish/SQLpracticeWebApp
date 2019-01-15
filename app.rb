f7ed402a8994b2c9ddb29d152252bb3a229347b6
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-15 22:57:36 +0900

    テンプレートの利用とCSSフレームワークの利用

--- app.rb
+++ app.rb
@@ -1,4 +1,4 @@
-require 'sinatra/base'
+require 'sinatra'
 require 'sinatra/reloader'
 require 'mysql2'
 require 'dotenv'
@@ -7,19 +7,20 @@ require 'erb'
 
 class SQLApplication < Sinatra::Base
 
-Dotenv.load
-yaml_file = File.read("./database.yml")
-sql_client = Mysql2::Client.new(YAML.load(ERB.new(yaml_file).result))
-  
+  Dotenv.load
+  yaml_file = File.read("./database.yml")
+  sql_client = Mysql2::Client.new(YAML.load(ERB.new(yaml_file).result))
+ 
   get '/' do
     @text = 'Welcome to Sinatra!!'
+    erb :index
 =begin
     query = "select * from test1"
     output = ""
     result = sql_client.query(query)
     result.each do |a|
       output =  a["text"]
-    end
+    end      
     return output
 =end
   end
