c576d8f4c9f22cdd883d8922c456e0817b1a55fa
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-19 01:03:29 +0900

    レイアウトの変更

--- app.rb
+++ app.rb
@@ -37,7 +37,7 @@ class SQLApplication < Sinatra::Base
       @answer_column = []
       @answer_rows = []
       answer.each do |row| 
-        @answer_colunm = row.keys
+        @answer_column = row.keys
         @answer_rows << row.values
       end
 
