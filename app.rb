8fc03e077327d7a9f283d15d9b39fa1cc7f757df
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-19 00:41:07 +0900

    problemページの作成

--- app.rb
+++ app.rb
@@ -27,22 +27,62 @@ class SQLApplication < Sinatra::Base
 
   get '/problems/:id' do
     query = "select * from Problems where id=?"
+    @id=params[:id]
     begin
       @result = sql_client.prepare(query).execute(params[:id])
       @result.each do |a|
-        p a
+        @problem = a
       end
-      "#{@result}"
+      answer = sql_client.query(@problem["answer"])
+      @answer_column = []
+      @answer_rows = []
+      answer.each do |row| 
+        @answer_colunm = row.keys
+        @answer_rows << row.values
+      end
+
     rescue
       @text = 'mysql error!'
     end
+
+    erb :problem
+    
+  end
+
+  post '/problems/:id' do
+    @id = params[:id]
+    begin
+      query = "select * from Problems where id=?"
+      @result = sql_client.prepare(query).execute(params[:id])
+      @result.each do |a|
+        @problem = a
+      end
+      answer = sql_client.query(@problem["answer"])
+      @answer_column = []
+      @answer_rows = []
+      answer.each do |row| 
+        @answer_colunm = row.keys
+        @answer_rows << row.values
+      end
+      result = sql_client.query(params["sqlquery"])
+      @player_column = []
+      @player_rows = []
+      result.each do |row| 
+        @player_column = row.keys
+        @player_rows << row.values
+      end
+    rescue
+      @text = 'mysql error!'
+    end
+     
+    erb :problem
   end
   get '/freesql' do
     query = "select * from Aqours"
     begin
-      result = sql_client.query(query)
+      @result = sql_client.query(query)
       @column_name = []
-      @rows = []
+      @prows = []
       result.each do |row| 
         @column_name = row.keys
         @rows << row.values
