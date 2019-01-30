c1c39ca86f602d7d7494a832507318a840574dcc
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-31 01:36:37 +0900

    テストを通るようにProblemモデルのバリデーションを記述

--- models/Problem.rb
+++ models/Problem.rb
@@ -5,5 +5,9 @@ config  = YAML.load(ERB.new(File.read("./database.yml")).result)
 ActiveRecord::Base.establish_connection(config[ENV['APP_ENV']])
 
 class Problem < ActiveRecord::Base
-
+  validates :id, presence: true
+  validates :title, presence: true
+  validates :statement, presence: true
+  validates :answer, presence: true
+  validates :level, presence: true
 end
