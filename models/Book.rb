1a12a9f4d8f658f389967527119ce9fd7ced6d26
takuminish <s15t253@stu.kagawa-u.ac.jp>
2019-01-30 23:25:48 +0900

    development環境とtest環境を変えれるようにした

--- models/Book.rb
+++ models/Book.rb
@@ -1,7 +1,8 @@
 require 'active_record'
 
 config  = YAML.load(ERB.new(File.read("./database.yml")).result)
-ActiveRecord::Base.establish_connection(config["test"])
+#ActiveRecord::Base.establish_connection(config["test"])
+ActiveRecord::Base.establish_connection(config[ENV['APP_ENV']])
 
 class Book < ActiveRecord::Base
 
